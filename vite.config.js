420a23bedd0079f660040d18afd86f1b943d8b2b
Max Ghenis <mghenis@gmail.com>
2024-12-12 00:30:16 -0500

    try again

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/manifold-stock-bot/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    outDir: 'dist',
   },
 })
